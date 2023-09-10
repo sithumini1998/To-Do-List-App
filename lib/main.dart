f827d3a304bf0a0791f5989d5133f0ce9ce454c9
sithumini1998 <98685539+sithumini1998@users.noreply.github.com>
2023-09-10 11:36:18 +0530

    adding  flutter native splash

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -93,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'You have pushed the button this many time:',
             ),
             Text(
               '$_counter',
